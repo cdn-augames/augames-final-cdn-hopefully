2f7b140b4461680322e2ff5fb347b684b6b642e1
realtherealer <147657739+realtherealer@users.noreply.github.com>
2025-06-04 18:54:58 +0100

    Update configure-ruffle.js

--- ruffle-swf-embed-main/ruffle-swf-embed-main/ruffle-testing/configure-ruffle.js
+++ ruffle-swf-embed-main/ruffle-swf-embed-main/ruffle-testing/configure-ruffle.js
@@ -9,7 +9,7 @@ const defaultConfig = {
 	"forceScale": true,
 	"forceAlign": true,
 };
-const defaultSWF = 'logo-anim.swf';
+const defaultSWF = 'https://cdn.jsdelivr.net/gh/cdn-augames/augames-final-cdn-hopefully@main/ruffle-swf-embed-main/ruffle-swf-embed-main/ruffle-testing/logo-anim.swf';
 const urlParams = new URLSearchParams(window.location.search);
 const swfName = urlParams.get('swf') || defaultSWF;
 urlParams.delete('swf'); // Not part of Ruffle config object
